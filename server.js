ff4f0d27b63a55e0020cca23d0830c52d9668628
Jeremiah <jeremiah.d.wise@gmail.com>
2022-08-29 17:39:17 -0500

    add models

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+
 const path = require('path')
+
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+
+const {logger, logEvents} = require('./middleware/logger')
+
+const errorHandler = require('./middleware/errorHandler')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+
+
+const app = express()
 const PORT = process.env.PORT || 3500
 
+console.log(process.env.NODE_ENV)
+
+connectDB()
+
+app.use(logger)
+
+app.use(cors(corsOptions))
+
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
+
+app.use('/users', require('./routes/userRoutes'))
 
 app.all('*', (req, res) =>{
   res.status(404)
@@ -20,4 +46,15 @@ app.all('*', (req, res) =>{
   }
 })
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+app.use(errorHandler)
+
+mongoose.connection.once('open', () =>{
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+  console.log(err)
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
+
